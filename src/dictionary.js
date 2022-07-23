ad7e3162b3fc5a8f67030eb27f19f85985b1a144
Seyitanx <seyitanadepegba@gmail.com>
2022-07-23 19:09:24 +0100

    showed the meanings with the use of Api

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Result from "./result"
 import "./dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-
-function showResponse(response) {
-  console.log(response.data[0])
-}
-
+let [results, setResults] = useState(null);
+  function showResponse(response) {
+   setResults(response.data[0]);
+    //console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
 
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(showResponse)
+    axios.get(apiUrl).then(showResponse);
   }
 
   function handleChange(event) {
@@ -26,6 +27,7 @@ function showResponse(response) {
       <form onSubmit={search}>
         <input type="search" onChange={handleChange} autoFocus={true} />
       </form>
+      <Result results={results}/>
     </div>
   );
 }

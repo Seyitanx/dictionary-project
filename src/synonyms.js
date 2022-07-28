7ad43715d7eab450f8465abff5854eb3b61d7cf4
Seyitanx <seyitanadepegba@gmail.com>
2022-07-28 21:45:52 +0100

    .

--- src/synonyms.js
+++ src/synonyms.js
@@ -1,19 +1,14 @@
 import React from "react";
 
-
 export default function Synonyms(props) {
-    if (props.synonyms) {
-        return(
-            <ul>
-       {props.synonyms.map(function (synonyms, index) {
-        return( 
-           <li key={index}> {synonyms}
-           </li>
-        )
-       })}</ul>
-    )
-    } else {
-        
-    }
-    
+  if (props.synonyms) {
+    return (
+      <ul className="synonyms">
+        {props.synonyms.map(function (synonyms, index) {
+          return <li key={index}> {synonyms}</li>;
+        })}
+      </ul>
+    );
+  } else {
+  }
 }
